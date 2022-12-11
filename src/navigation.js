63f676852130ea2dca02765e18bda15d7a977386
Flexx <juandavidx40@gmail.com>
2022-12-11 16:42:38 -0500

    Add navigator.js and Methods}

---
+++ src/navigation.js
@@ -0,0 +1,41 @@
+window.addEventListener('DOMContentLoaded', navigator, false);
+window.addEventListener('hashchange', navigator, false);
+
+function navigator(){
+    console.log(location)
+
+    if(location.hash.startsWith('#trends')){
+        trendsPage()
+    }else if(location.hash.startsWith('#search=')){
+        searchPage()
+    }else if(location.hash.startsWith('#movie=')){
+        movieDetailsPage()
+    }else if(location.hash.startsWith('#category=')){
+        categoriesPage()
+    }else {
+        homePage()
+    }
+}
+
+function homePage(){
+    console.log("home")
+    getTrandingMoviesPreview()
+getCategoriesPreview()
+}
+
+function categoriesPage(){
+    console.log("categorie")
+
+}
+
+function movieDetailsPage(){
+    console.log("movieDeatils")
+}
+
+function searchPage(){
+    console.log("searchPage")
+}
+
+function trendsPage(){
+    console.log("trends")
+}
